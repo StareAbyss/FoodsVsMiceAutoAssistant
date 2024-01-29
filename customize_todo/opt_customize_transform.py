6e1d7896e2fb7c76bd177d8f327f822f02c406ef
StareAbyss <815204388@qq.com>
2024-01-29 21:38:05 +0800

    v0.9.0 修改了自定义方案生成函数和示例方案, 以适配新版

--- customize_todo/opt_customize_transform.py
+++ customize_todo/opt_customize_transform.py
@@ -5,20 +5,22 @@ from pprint import pprint
 from function.script.scattered.read_json_to_stage_info import read_json_to_stage_info
 
 csvFilePath = "csv_美食大赛.csv"
-jsonFilePath = "231221-240118 大赛单人.json"
+jsonFilePath = "1 - 231221-240118 大赛.json"
 
 my_list = []
 
-default_deck = ["炭烧海星",
-                "小火炉",
-                "糖葫芦炮弹",
-                "瓜皮护罩",
-                "狮子座精灵",
-                "油灯",
-                "樱桃反弹布丁",
-                "苏打气泡",
-                "木盘子",
-                "麦芽糖"]
+default_deck = [
+    "炭烧海星",
+    "小火炉",
+    "糖葫芦炮弹",
+    "瓜皮护罩",
+    "狮子座精灵",
+    "油灯",
+    "樱桃反弹布丁",
+    "苏打气泡",
+    "木盘子",
+    "麦芽糖"
+]
 
 with open(csvFilePath, "r", encoding='utf-8') as csv_file:
     # 读取文本为有序字典
@@ -33,25 +35,29 @@ for my_dict in my_list:
     my_dict.pop("产火数")
 
     my_dict["battle_id"] = int(my_dict.pop("任务序号"))
-    my_dict["quest_card"] = my_dict.pop("使用卡片")
+
     my_dict["stage_id"] = my_dict.pop("地图编号")
-
     my_dict["max_times"] = 1
+    my_dict["deck"] = 1
 
-    my_dict["is_group"] = my_dict.pop("是否组队")
-    if my_dict["is_group"] == "True":
-        my_dict["is_group"] = True
+    my_dict["player"] = my_dict.pop("是否组队")
+    if my_dict["player"] == "True":
+        my_dict["player"] = [2,1]
     else:
-        my_dict["is_group"] = False
+        my_dict["player"] = [1]
 
+    my_dict["battle_plan_1p"] = 0
+    my_dict["battle_plan_2p"] = 1
+
+    my_dict["quest_card"] = my_dict.pop("使用卡片")
+
+    """ban卡"""
     my_dict["list_ban_card"] = my_dict.pop("不使用卡片")
     my_dict["list_ban_card"] = my_dict["list_ban_card"].split(",")
-
-    """去list的空值"""
+    # 去list的空值
     if "" in my_dict["list_ban_card"]:
         my_dict["list_ban_card"].remove("")
-
-    """根据数量限制 地图所需的承载卡 优先级 综合规划 增加ban卡"""
+    # 根据数量限制 地图所需的承载卡 优先级 综合规划 增加ban卡
     if not (my_dict["数量限制"] == "0" or my_dict["数量限制"] == ""):
         # 先把已经ban了的 从卡组中 去掉
         for already_ban in my_dict["list_ban_card"]:
@@ -70,19 +76,12 @@ for my_dict in my_list:
             my_dict["list_ban_card"].append(j)
     my_dict.pop("数量限制")
 
-    """增加额外的预设值"""
-
-    my_dict["deck"] = 1
-
-    my_dict["battle_plan_1p"] = 0
-
-    my_dict["battle_plan_2p"] = 1
-
     my_dict["dict_exit"] = {
-        "other_time": ["none"],
-        "last_time": ["food_competition"]}
-
-    """"""
+        "other_time_player_a": [],
+        "other_time_player_b": [],
+        "last_time_player_a": ["美食大赛领取"],
+        "last_time_player_b": ["美食大赛领取"]
+    }
 
 pprint(my_list)
 
