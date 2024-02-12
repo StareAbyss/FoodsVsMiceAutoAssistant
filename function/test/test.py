16c631cca943a5989a7d99ef30900cec45882288
StareAbyss <815204388@qq.com>
2024-02-12 12:13:17 +0800

    v0.9.1 test函数的集中搬迁

--- function/test/test.py
+++
@@ -1,6 +0,0 @@
-import time
-
-timer_begin = time.time()
-t = (time.time() - timer_begin)
-
-print(t)
--- test/test_compare_picture.py
+++ test/test_compare_picture.py
@@ -1,5 +1,8 @@
 import cProfile
 
+import cv2
+import numpy as np
+
 from function.common.bg_p_compare import find_p_in_w
 from function.get_paths import paths
 from function.script.scattered.gat_handle import faa_get_handle
@@ -7,16 +10,16 @@ from function.script.scattered.gat_handle import faa_get_handle
 
 def f_test():
     handle = faa_get_handle(channel="深渊之下 | 锑食", mode="flash")
-    target_path = paths["picture"]["common"] + "\\底部菜单\\跳转_竞技场.png"
-    # target_path = cv2.imdecode(np.fromfile(target_path, dtype=np.uint8), -1)
-    for i in range(100):
-        find_p_in_w(
-            raw_w_handle=handle,
-            # raw_range=[0, 0, 2000, 2000],
-            raw_range=[0, 0, 950, 600],
-            target_path=target_path,
-            target_tolerance=0.95
-        )
+    target_path = paths["picture"]["item"] + "\\原始资源\\2\\1级四叶草.png"
+    target_path = cv2.imdecode(np.fromfile(target_path, dtype=np.uint8), -1)
+    print(target_path)
+    # for i in range(100):
+    find_p_in_w(
+        raw_w_handle=handle,
+        raw_range=[0, 0, 950, 600],
+        target_path=target_path,
+        target_tolerance=0.95
+    )
 
 
 cProfile.run("f_test()")
