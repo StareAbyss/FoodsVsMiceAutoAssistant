f15138a2b78249af4063d33355bdfefc01705538
StareAbyss <815204388@qq.com>
2024-03-19 19:18:05 +0800

    v1.1.0 略微修改了界面输出框的初始输出, 修正了错误的B站视频号, 修改了初等用户指南名称(使用前请看我->FAA从入门到神殿), 移除冗余函数若干

--- function/script/ui_0_load_ui_file.py
+++ function/script/ui_0_load_ui_file.py
@@ -53,15 +53,16 @@ class MyMainWindow0(QMainWindow):
 
     def start_print(self):
         """打印默认输出提示"""
+        self.printf("嗷呜, 欢迎使用FAA-美食大战老鼠自动放卡作战小助手~")
         self.printf("使用安全说明")
         self.printf("[1] 务必有二级密码")
         self.printf("[2] 有一定的礼卷防翻牌异常")
         self.printf("[3] 高星或珍贵不绑卡挂拍卖/提前转移")
         self.printf("")
-        self.printf("任何用户使用前, 请认真阅读[使用前请看我.pdf], 以解决[运行直接闪退][开始后没反应]多数问题")
+        self.printf("用户请认真阅读[FAA从入门到神殿.pdf], 以解决[运行直接闪退][开始后没反应][缩放炸裂]等多数问题")
         self.printf("开发者和深入使用, 请参考[README.md]")
         self.printf("[Github] https://github.com/StareAbyss/FoodsVsMouses_AutoAssistant")
-        self.printf("[B站] https://www.bilibili.com/video/BV1fS421N7")
+        self.printf("[B站][UP直视深淵][老版宣传视频]https://www.bilibili.com/video/BV1fS421N7zf")
         self.printf("[反馈&交流QQ] 786921130 欢迎加入获取帮助")
         self.printf("[开源][免费][绿色] 请为我在 Github点个Star/B站三连评论弹幕支持吧")
 
@@ -69,21 +70,6 @@ class MyMainWindow0(QMainWindow):
         """
         对MainWindow的函数closeEvent进行重构, 退出软件时弹窗提醒 并且结束所有进程(和内部的线程)
         """
-
-        # 退出提示
-        # self.reply = QMessageBox.question(
-        #     self,
-        #     '提示',
-        #     "确认退出吗？",
-        #     QMessageBox.Yes | QMessageBox.No,
-        #     QMessageBox.No
-        # )
-        # if self.reply == QMessageBox.Yes:
-        #     event.accept()
-        #     # 用过sys.exit(0)和sys.exit(app.exec_())，但没起效果
-        #     os._exit(0)
-        # else:
-        #     event.ignore()
 
         event.accept()
         # 用过sys.exit(0)和sys.exit(app.exec_())，但没起效果
