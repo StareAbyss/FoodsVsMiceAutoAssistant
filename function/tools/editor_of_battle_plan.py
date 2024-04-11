4793144b2fcb02d52d6316ceeba577152b0b7625
StareAbyss <815204388@qq.com>
2024-04-11 10:50:17 +0800

    v1.3.0 大幅修改战斗方案编辑器; 1. 支持撤销/重做快捷键; 2. 对棋盘按钮添加鼠标提示信息，提示具体位置x-x; 3. 删除更新按钮，现在对属性的编辑都能实时保存; 4. 右键按钮可以删除当前按钮的全部卡片; 5. 幻幻鸡参数适配，美化list显示; 6. 高亮当前选中卡片所在棋盘格

--- function/tools/editor_of_battle_plan.py
+++ function/tools/editor_of_battle_plan.py
@@ -1,10 +1,14 @@
+import copy
 import json
 import sys
 
-from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtCore import pyqtSignal, Qt
+from PyQt5.QtGui import QKeySequence
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QGridLayout, QPushButton, QWidget, QFileDialog, QVBoxLayout, QLabel, QComboBox,
-    QLineEdit, QHBoxLayout, QTextEdit, QListWidget, QMessageBox)
+    QLineEdit, QHBoxLayout, QTextEdit, QListWidget, QMessageBox, QSpinBox, QListWidgetItem, QShortcut, QFrame)
+
+from function.globals.get_paths import PATHS
 
 double_click_card_list = pyqtSignal(object)
 
@@ -42,7 +46,7 @@ class QListWidgetDraggable(QListWidget):
 
     def mousePressEvent(self, e):
         super().mousePressEvent(e)
-        print("鼠标点击事件触发")
+        # print("鼠标点击事件触发")
         current_row = self.currentRow()
         if current_row == 0:
             # 禁止拖拽
@@ -57,6 +61,9 @@ class JsonEditor(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        """字体"""
+        self.my_font = None
+
         """内部数据表"""
         # 数据dict
         self.json_data = {
@@ -67,6 +74,14 @@ class JsonEditor(QMainWindow):
 
         # 当前被选中正在编辑的项目的index
         self.current_edit_index = None
+
+        # 撤销/重做功能
+        self.undo_stack = []
+        self.redo_stack = []
+        self.undo_shortcut = QShortcut(QKeySequence('Ctrl+Z'), self)
+        self.undo_shortcut.activated.connect(self.undo)
+        self.redo_shortcut = QShortcut(QKeySequence('Ctrl+Y'), self)
+        self.redo_shortcut.activated.connect(self.redo)
 
         """布局和控件放置"""
 
@@ -102,7 +117,7 @@ class JsonEditor(QMainWindow):
         self.LayPrint.addWidget(self.WeiCurrentEdit)
 
         self.LayPrint.addWidget(QLabel(
-            '双击左侧列表, 选中对象进行编辑 | 点击格子添加到该位置, 再点一下取消位置 | 列表从上到下 是一轮放卡的优先级(人物除外)'))
+            '点击左侧列表, 选中对象进行编辑 | 点击格子添加到该位置, 再点一下取消位置 | 列表从上到下 是一轮放卡的优先级(人物除外)'))
         """卡片编辑器"""
         self.LayCardEditor = QHBoxLayout()
         self.LayMain.addLayout(self.LayCardEditor)
@@ -133,11 +148,14 @@ class JsonEditor(QMainWindow):
         self.LayCardEditor.addWidget(self.QueueInput)
         self.QueueInput.setToolTip("队列和遍历不知道是什么可以全true, 具体请参见详细文档")
 
+        self.LayCardEditor.addWidget(QLabel('幻鸡优先级:'))
+        self.KunInput = QSpinBox()
+        self.KunInput.setRange(0, 10)
+        self.LayCardEditor.addWidget(self.KunInput)
+        self.KunInput.setToolTip("卡片是否使用幻幻鸡, 0代表不使用，越高使用优先级越高")
+
         self.WeiAddCardButton = QPushButton('添加 一张卡片')
         self.LayCardEditor.addWidget(self.WeiAddCardButton)
-
-        self.WeiUpdateCardButton = QPushButton('更新 选中卡片')
-        self.LayCardEditor.addWidget(self.WeiUpdateCardButton)
 
         self.WeiDeleteCardButton = QPushButton('删除 选中卡片')
         self.LayCardEditor.addWidget(self.WeiDeleteCardButton)
@@ -155,15 +173,33 @@ class JsonEditor(QMainWindow):
         self.LayCardListAndCell.addLayout(self.chessboard_layout)
         # 生成棋盘布局中的元素
         self.chessboard_buttons = []
+        self.chessboard_frames = []  # 用于存储QFrame的列表
+
         for i in range(7):
-            row = []
+            row_buttons = []
+            row_frames = []
+
             for j in range(9):
-                btn = QPushButton('')
+                btn = ChessButton('')
                 btn.setFixedSize(100, 100)
                 btn.clicked.connect(lambda checked, x=i, y=j: self.place_card(y, x))
+                btn.rightClicked.connect(lambda x=i, y=j: self.remove_card(y, x))
                 self.chessboard_layout.addWidget(btn, i, j)
-                row.append(btn)
-            self.chessboard_buttons.append(row)
+                btn.setToolTip(f"当前位置: {j + 1}-{i + 1}")
+                row_buttons.append(btn)
+
+                # 创建QFrame作为高亮效果的载体
+                frame = QFrame(self)
+                frame.setFixedSize(100, 100)
+                frame.setFrameShape(QFrame.StyledPanel)
+                frame.setFrameShadow(QFrame.Raised)
+                self.chessboard_layout.addWidget(frame, i, j)
+                frame.lower()  # 确保QFrame在按钮下方
+                frame.setAttribute(Qt.WA_TransparentForMouseEvents, True)  # 防止遮挡按钮
+                row_frames.append(frame)
+
+            self.chessboard_buttons.append(row_buttons)
+            self.chessboard_frames.append(row_frames)
 
         """保存按钮"""
         self.save_button = QPushButton('保存战斗方案')
@@ -184,13 +220,16 @@ class JsonEditor(QMainWindow):
         # 添加卡片
         self.WeiAddCardButton.clicked.connect(self.add_card)
 
-        # 更新卡片
-        self.WeiUpdateCardButton.clicked.connect(self.update_card)
-
         # 删除卡片
         self.WeiDeleteCardButton.clicked.connect(self.delete_card)
-        # 双击列表更改当前card
-        self.WeiCardList.itemDoubleClicked.connect(self.current_card_change)
+        # 单击列表更改当前card
+        self.WeiCardList.itemClicked.connect(self.current_card_change)
+        # id，名称,遍历，队列控件的更改都连接上更新卡片
+        self.WeiIdInput.textChanged.connect(self.update_card)
+        self.WeiNameInput.textChanged.connect(self.update_card)
+        self.WeiErgodicInput.currentIndexChanged.connect(self.update_card)
+        self.QueueInput.currentIndexChanged.connect(self.update_card)
+        self.KunInput.valueChanged.connect(self.update_card)
 
         """外观"""
         self.UICss()
@@ -201,26 +240,37 @@ class JsonEditor(QMainWindow):
     def highlight_chessboard(self, card_locations):
         """根据卡片的位置list，将对应元素的按钮进行高亮"""
         # 清除所有按钮的高亮
-        for row in self.chessboard_buttons:
+        for row in self.chessboard_frames:
             for btn in row:
+                # 去除高亮
                 btn.setStyleSheet("")
+
 
         # 高亮显示选中卡片的位置
         for location in card_locations:
             x, y = map(int, location.split('-'))
-            self.chessboard_buttons[y - 1][x - 1].setStyleSheet("background-color: orange;")
+             # 添加背景颜色到现有样式
+            self.chessboard_frames[y - 1][x - 1].setStyleSheet("background-color: rgba(255, 255, 0, 127);")
+
 
     def UICss(self):
         # 窗口名
         self.setWindowTitle('战斗方案编辑器')
 
     def current_card_change(self, item):
-        """被双击后, 被选中的卡片改变了"""
-        index = self.WeiCardList.indexFromItem(item).row()  # list的index 是 QModelIndex 此处还需要获取到行号
+        """被单击后, 被选中的卡片改变了"""
+        self.index = self.WeiCardList.indexFromItem(item).row()  # list的index 是 QModelIndex 此处还需要获取到行号
 
-        self.current_edit_index = index
+        self.current_edit_index = self.index
 
-        if index == 0:
+        # 为输入控件信号上锁，在初始化时不会触发保存
+        self.WeiCurrentEdit.blockSignals(True)
+        self.WeiIdInput.blockSignals(True)
+        self.WeiNameInput.blockSignals(True)
+        self.WeiErgodicInput.blockSignals(True)
+        self.QueueInput.blockSignals(True)
+
+        if self.index == 0:
             # 玩家 直接清空它们
             self.WeiCurrentEdit.setText("玩家")
             self.WeiIdInput.clear()
@@ -228,41 +278,61 @@ class JsonEditor(QMainWindow):
             self.WeiNameInput.setText("玩家")
             self.WeiErgodicInput.setCurrentIndex(0)
             self.QueueInput.setCurrentIndex(0)
+            self.KunInput.setValue(0)
             self.highlight_chessboard(self.json_data["player"])
         else:
             # 卡片
-            index = index - 1  # 可能需要深拷贝？也许是被保护的特性 不需要
+            index = self.index - 1  # 可能需要深拷贝？也许是被保护的特性 不需要
             card = self.json_data["card"][index]
             self.WeiCurrentEdit.setText("索引-{} 名称-{}".format(index, card["name"]))
             self.WeiIdInput.setText(str(card['id']))
             self.WeiNameInput.setText(card['name'])
             self.WeiErgodicInput.setCurrentText(str(card['ergodic']).lower())
             self.QueueInput.setCurrentText(str(card['queue']).lower())
+            self.KunInput.setValue(card.get('kun', 0))
             # 设置高亮
             self.highlight_chessboard(card["location"])
+
+        # 解锁控件信号
+        self.WeiCurrentEdit.blockSignals(False)
+        self.WeiIdInput.blockSignals(False)
+        self.WeiNameInput.blockSignals(False)
+        self.WeiErgodicInput.blockSignals(False)
+        self.QueueInput.blockSignals(False)
 
     def load_data_to_ui_list(self):
         """从 [内部数据表] 载入数据到 [ui的list]"""
         self.WeiCardList.clear()
-        my_list = ["玩家"]
+
+        player_item = QListWidgetItem("玩家")
+        self.WeiCardList.addItem(player_item)
+
+        # 找到最长的卡片名称长度
+        if self.json_data["card"]:
+            max_name_length = max(len(card["name"]) for card in self.json_data["card"])
+
         for card in self.json_data["card"]:
-            my_list.append(
-                "遍历:{} 队列:{} ID:{} 名称:{} ".format(
-                    "√" if card["ergodic"] else "X",
-                    "√" if card["queue"] else "X",
-                    card["id"],
-                    card["name"]
-                )
+            # 使用制表符或空格填充卡片名称
+            padded_name = card["name"].ljust(max_name_length)
+
+            text = "{}\tID:{} 遍历:{} 队列:{}".format(
+                padded_name,
+                card["id"],
+                "√" if card["ergodic"] else "X",
+                "√" if card["queue"] else "X"
             )
-        self.WeiCardList.addItems(my_list)
-        self.current_edit_index = None  # 正在编辑者选为None
-        self.WeiCurrentEdit.setText("无")
-        print(my_list)
+            if card.get("kun", 0):
+                text += " 坤:{}".format(card["kun"])
+            item = QListWidgetItem(text)
+            self.WeiCardList.addItem(item)
 
     def card_list_be_dropped(self, index_from, index_to):
         """在list的drop事件中调用, 用于更新内部数据表"""
         cards = self.json_data["card"]
         if index_to != 0:
+            # 将当前状态压入栈中
+            self.append_undo_stack()
+
             change_card = cards.pop(index_from - 1)
             print(change_card)
             cards.insert(index_to - 1, change_card)
@@ -305,11 +375,18 @@ class JsonEditor(QMainWindow):
             QMessageBox.information(self, "操作错误！", "该对象(玩家)不能编辑信息, 只能设置位置!")
             return False
 
+        # 将当前状态压入栈中
+        self.append_undo_stack()
+
         index -= 1
-        self.json_data["card"][index]["id"] = int(self.WeiIdInput.text())
-        self.json_data["card"][index]["name"] = self.WeiNameInput.text()
-        self.json_data["card"][index]["ergodic"] = bool(self.WeiErgodicInput.currentText() == 'true')  # 转化bool
-        self.json_data["card"][index]["queue"] = bool(self.QueueInput.currentText() == 'true')  # 转化bool
+        card = self.json_data["card"][index]
+        id = int(self.WeiIdInput.text())
+        card["id"] = id
+        card["name"] = self.WeiNameInput.text()
+        card["ergodic"] = bool(self.WeiErgodicInput.currentText() == 'true')  # 转化bool
+        card["queue"] = bool(self.QueueInput.currentText() == 'true')  # 转化bool
+        card["kun"] = self.KunInput.value()
+        self.WeiCurrentEdit.setText("索引-{} 名称-{}".format(self.index - 1, card["name"]))
         self.load_data_to_ui_list()
         self.refresh_chessboard()
 
@@ -322,6 +399,8 @@ class JsonEditor(QMainWindow):
         if index == 0:
             QMessageBox.information(self, "操作错误！", "该对象(玩家)不能编辑信息, 只能设置位置!")
             return False
+        # 将当前状态压入栈中
+        self.append_undo_stack()
 
         index -= 1
         del self.json_data["card"][index]
@@ -329,34 +408,57 @@ class JsonEditor(QMainWindow):
         self.refresh_chessboard()
 
     def place_card(self, x, y):
-        if self.current_edit_index is not None:
-            # 初始化 并非没有选择任何东西
-            location_key = f"{x + 1}-{y + 1}"
-
-            if self.current_edit_index == 0:
-                # 当前index为玩家
-                target = self.json_data["player"]
-                # 如果这个位置已经有了玩家，那么移除它；否则添加它
-                if location_key in target:
-                    target.remove(location_key)
-                else:
-                    target.append(location_key)
-                self.refresh_chessboard()
+        if self.current_edit_index is None:
+            return
+        # 将当前状态压入栈中
+        self.append_undo_stack()
+        # 初始化 并非没有选择任何东西
+        location_key = f"{x + 1}-{y + 1}"
+        if self.current_edit_index == 0:
+            # 当前index为玩家
+            target = self.json_data["player"]
+            # 如果这个位置已经有了玩家，那么移除它；否则添加它
+            if location_key in target:
+                target.remove(location_key)
             else:
-                # 当前index为卡片
-                target = self.json_data["card"][self.current_edit_index - 1]
+                target.append(location_key)
+            self.refresh_chessboard()
+        else:
+            # 当前index为卡片
+            target = self.json_data["card"][self.current_edit_index - 1]
+            # 如果这个位置已经有了这张卡片，那么移除它；否则添加它
+            if location_key in target['location']:
+                target['location'].remove(location_key)
+            else:
+                target['location'].append(location_key)
+            self.refresh_chessboard()
 
-                # 如果这个位置已经有了这张卡片，那么移除它；否则添加它
-                if location_key in target['location']:
-                    target['location'].remove(location_key)
-                else:
-                    target['location'].append(location_key)
-                self.refresh_chessboard()
+    def remove_card(self, x, y):
+        # 将当前状态压入栈中
+        self.append_undo_stack()
+        # 删除当前位置上的所有卡片
+        location = f"{x + 1}-{y + 1}"
+        # 从玩家位置中删除
+        if location in self.json_data["player"]:
+            self.json_data["player"].remove(location)
+        # 从卡片位置中删除
+        for card in self.json_data["card"]:
+            if location in card["location"]:
+                card["location"].remove(location)
+        # 更新界面显示
+        self.refresh_chessboard()
 
     def save_json(self):
         self.json_data['tips'] = self.WeiTipsEditor.toPlainText()
         options = QFileDialog.Options()
-        file_name, _ = QFileDialog.getSaveFileName(self, "保存 JSON 文件", "", "JSON Files (*.json)", options=options)
+
+        file_name, _ = QFileDialog.getSaveFileName(
+            parent=self,
+            caption="保存 JSON 文件",
+            directory=PATHS["battle_plan"],
+            filter="JSON Files (*.json)",
+            options=options)
+
         if file_name:
             with open(file_name, 'w', encoding='utf-8') as file:
                 # 确保玩家位置也被保存
@@ -365,8 +467,22 @@ class JsonEditor(QMainWindow):
 
     def load_json(self):
         """打开窗口 读取josn文件"""
+        # 为输入控件信号上锁，在初始化时不会触发保存
+        self.WeiCurrentEdit.blockSignals(True)
+        self.WeiIdInput.blockSignals(True)
+        self.WeiNameInput.blockSignals(True)
+        self.WeiErgodicInput.blockSignals(True)
+        self.QueueInput.blockSignals(True)
+
         options = QFileDialog.Options()
-        file_name, _ = QFileDialog.getOpenFileName(self, "打开 JSON 文件", "", "JSON Files (*.json)", options=options)
+
+        file_name, _ = QFileDialog.getOpenFileName(
+            parent=self,
+            caption="打开 JSON 文件",
+            directory=PATHS["battle_plan"],
+            filter="JSON Files (*.json)",
+            options=options)
+
         if file_name:
             with open(file_name, 'r', encoding='utf-8') as file:
                 self.json_data = json.load(file)
@@ -383,6 +499,18 @@ class JsonEditor(QMainWindow):
                 # 根据数据绘制视图
                 self.load_data_to_ui_list()
                 self.refresh_chessboard()
+                # 清除所有按钮的高亮
+                for row in self.chessboard_frames:
+                    for btn in row:
+                        # 去除高亮
+                        btn.setStyleSheet("")
+
+        # 为输入控件信号解锁
+        self.WeiCurrentEdit.blockSignals(False)
+        self.WeiIdInput.blockSignals(False)
+        self.WeiNameInput.blockSignals(False)
+        self.WeiErgodicInput.blockSignals(False)
+        self.QueueInput.blockSignals(False)
 
     def refresh_chessboard(self):
         """刷新棋盘上的文本等各种元素"""
@@ -411,6 +539,49 @@ class JsonEditor(QMainWindow):
                     else:
                         text += " {}".format(c_index_list)
                 btn.setText(text)
+
+    def append_undo_stack(self):
+        # 将当前状态压入栈中
+        self.undo_stack.append(copy.deepcopy(self.json_data))
+        # 清空重做栈
+        self.redo_stack.clear()
+        # 只保留20次操作
+        if len(self.undo_stack) > 20:
+            self.undo_stack.pop(0)
+
+    def undo(self):
+        if len(self.undo_stack) > 0:
+            current_state = copy.deepcopy(self.json_data)
+            self.redo_stack.append(current_state)
+            self.json_data = self.undo_stack.pop()
+            self.load_data_to_ui_list()
+            self.refresh_chessboard()
+
+    def redo(self):
+        if len(self.redo_stack) > 0:
+            current_state = copy.deepcopy(self.json_data)
+            self.undo_stack.append(current_state)
+            self.json_data = self.redo_stack.pop()
+            self.load_data_to_ui_list()
+            self.refresh_chessboard()
+
+    def set_my_font(self,my_font):
+        self.my_font = my_font
+        self.setFont(self.my_font)
+
+
+class ChessButton(QPushButton):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.RightButton:
+            # 发出自定义的右键点击信号
+            self.rightClicked.emit()
+        else:
+            super().mousePressEvent(event)
+
+    rightClicked = pyqtSignal()
 
 
 if __name__ == '__main__':
