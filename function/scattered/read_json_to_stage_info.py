70cefd8d2aff1a3ed8b36900e793bcaaad89d727
StareAbyss <815204388@qq.com>
2024-04-25 21:36:03 +0800

    v1.3.1 新增配置文件 stage_info_extra.json 从 stage_info.json 中分离了游戏每个月都要变化的悬赏任务关卡和假期副本关卡的配置文件, 以方便更换, 并更新了配套的读取函数.

--- function/scattered/read_json_to_stage_info.py
+++ function/scattered/read_json_to_stage_info.py
@@ -1,29 +1,34 @@
 import json
 
 from function.globals.get_paths import PATHS
+from function.globals.log import CUS_LOGGER
 
 
 def read_json_to_stage_info(stage_id):
     """读取文件中是否存在预设"""
-    with open(PATHS["config"] + "//opt_stage_info.json", "r", encoding="UTF-8") as file:
-        f_my_dict = json.load(file)
+    with open(PATHS["config"] + "//stage_info.json", "r", encoding="UTF-8") as file:
+        stages_info = json.load(file)
+    with open(PATHS["config"] + "//stage_info_extra.json", "r", encoding="UTF-8") as file:
+        stages_info_extra = json.load(file)
 
     # 初始化
-    stage_info = f_my_dict["default"]
+    stage_info = stages_info["default"]
     stage_info["id"] = stage_id
 
     # 拆分关卡名称
-    stage_list = stage_id.split("-")
-    stage_0 = stage_list[0]  # type
-    stage_1 = stage_list[1]  # map
-    stage_2 = stage_list[2]  # stage
+    stage_0, stage_1, stage_2 = stage_id.split("-")  # type map stage
+
     # 如果找到预设
-    if stage_0 in f_my_dict.keys():
-        if stage_1 in f_my_dict[stage_0].keys():
-            if stage_2 in f_my_dict[stage_0][stage_1].keys():
-                # 用设定里有的键值对覆盖已有的 并填写关卡名称(没有则保持默认)
-                f_stage_info_1 = f_my_dict[stage_0][stage_1][stage_2]
+    for information in [stages_info, stages_info_extra]:
+        try_stage_info = information.get(stage_0, {}).get(stage_1, {}).get(stage_2, None)
+        print(try_stage_info)
+        if try_stage_info:
+            stage_info = {**stage_info, **try_stage_info}
+            break
 
-                stage_info = {**stage_info, **f_stage_info_1}
-
+    CUS_LOGGER.info("读取关卡信息: {}".format(stage_info))
     return stage_info
+
+
+if __name__ == '__main__':
+    read_json_to_stage_info(stage_id="OR-0-2")
