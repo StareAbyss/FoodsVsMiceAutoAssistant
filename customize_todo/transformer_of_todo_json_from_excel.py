70280816596c6e9408b414e297f5e1bfa489c360
StareAbyss <815204388@qq.com>
2024-06-29 15:34:06 +0800

    v1.4.0-beta.4 自定义作战序列. 重新维护了自行一作战序列, 以适配目前的各项定义, 包括: 更新了 自动转换excel为json的.py文件 和 excel模板; 更新了 todo_example.json 模板文件; 重写简易文档.

--- customize_todo/transformer_of_todo_json_from_excel.py
+++ customize_todo/transformer_of_todo_json_from_excel.py
@@ -1,3 +1,4 @@
+import copy
 import json
 import time
 from pprint import pprint
@@ -9,6 +10,7 @@ from function.globals.extra import EXTRA_GLOBALS
 from function.scattered.read_json_to_stage_info import read_json_to_stage_info
 
 
+# 可能需要pip install openpyxl
 # 该工具可能会需要一些额外的依赖项, 用于直接用excel生成cus_todo.json
 
 def main():
@@ -34,62 +36,55 @@ def main():
     df = df.T
     data_dict = df.to_dict()
     data_list = [a_dict for a_dict in data_dict.values()]
-
-    # pprint(data_list)
-
+    data_list_2 = []
     # 遍历每一行
-    for a_dict in data_list:
+    for excel_line in data_list:
 
-        # 去除无用列
-        a_dict.pop("产火数")
-        a_dict.pop("评分要求")
-
-        # 默认参数
-        a_dict["deck"] = 1
-        a_dict["is_use_key"] = True
-        a_dict["max_times"] = 1
-        a_dict["dict_exit"] = {
-            "other_time_player_a": [],
-            "other_time_player_b": [],
-            "last_time_player_a": ["回到上一级", "美食大赛领取"],
-            "last_time_player_b": ["回到上一级", "美食大赛领取"]
+        json_line = {
+            # 默认参数
+            "deck": 1,
+            "max_times": 1,
+            "battle_plan_1p": 0,
+            "battle_plan_2p": 1,
+            "dict_exit": {
+                "other_time_player_a": [],
+                "other_time_player_b": [],
+                "last_time_player_a": ["回到上一级", "美食大赛领取"],
+                "last_time_player_b": ["回到上一级", "美食大赛领取"]
+            },
+            # 直接读取参数
+            "stage_id": copy.deepcopy(excel_line["地图编号"]),
+            "battle_id": copy.deepcopy(int(excel_line["任务序号"])),
+            "quest_card": copy.deepcopy(excel_line["使用卡片"]),
+            "need_key": copy.deepcopy(excel_line["使用钥匙"])
         }
 
-        # 直接读取参数
-        a_dict["stage_id"] = a_dict.pop("地图编号")
-        a_dict["battle_id"] = int(a_dict.pop("任务序号"))
-        a_dict["quest_card"] = a_dict.pop("使用卡片")
-
         # transformer value of group
-        a_dict["player"] = a_dict.pop("是否组队")  # this type already is bool
-        if a_dict["player"]:
-            a_dict["player"] = [2, 1]
+        if excel_line["是否组队"]:
+            json_line["player"] = [2, 1]
         else:
-            a_dict["player"] = [1]
-
-        a_dict["battle_plan_1p"] = 0
-        a_dict["battle_plan_2p"] = 1
+            json_line["player"] = [1]
 
         """ban卡"""
-        a_dict["list_ban_card"] = a_dict.pop("不使用卡片")
+        ban_card_list_str = copy.deepcopy(excel_line["不使用卡片"])
 
-        if type(a_dict["list_ban_card"]) is float:
+        if type(ban_card_list_str) is float:
             # 值为float(即nan), 改为空List
-            a_dict["list_ban_card"] = []
+            json_line["ban_card_list"] = []
         else:
             # 值为str(不为nan), 根据逗号, 切割成列表
-            a_dict["list_ban_card"] = a_dict["list_ban_card"].split(",")
+            json_line["ban_card_list"] = ban_card_list_str.split(",")
 
         # 根据数量限制 地图所需的承载卡 优先级 综合规划 增加ban卡
-        if not np.isnan(a_dict["数量限制"]):
+        if not np.isnan(excel_line["数量限制"]):
 
             # 先把已经ban了的 从卡组中 去掉
-            for already_ban in a_dict["list_ban_card"]:
+            for already_ban in json_line["ban_card_list"]:
                 if already_ban in default_deck:
                     default_deck.remove(already_ban)
 
             # 根据地图所需的承载卡, 重新排序
-            mat_card_opt = read_json_to_stage_info(stage_id=a_dict["stage_id"])["mat_card"]
+            mat_card_opt = read_json_to_stage_info(stage_id=json_line["stage_id"])["mat_card"]
             if mat_card_opt == 1:
                 default_deck.remove("木盘子")
                 default_deck.insert(0, "木盘子")
@@ -100,21 +95,21 @@ def main():
                 default_deck.insert(0, "咖啡粉")
 
             # 在属于的卡中, 选出后几位ban掉, 如果有任务卡额外加一张ban掉
-            max_card_num = int(a_dict["数量限制"]) + 0 if a_dict["quest_card"] == "None" else 1
-            a_dict["list_ban_card"] = default_deck[max_card_num:]
+            max_card_num = int(excel_line["数量限制"]) + 0 if json_line["quest_card"] == "None" else 1
+            json_line["ban_card_list"] = default_deck[max_card_num:]
 
-        # 卸磨杀驴数量限制列
-        a_dict.pop("数量限制")
+        data_list_2.append(json_line)
 
-    pprint(data_list)
+    pprint(data_list_2)
 
     # 将list 保存为 json 自旋锁读写, 防止多线程读写问题
     while EXTRA_GLOBALS.file_is_reading_or_writing:
         time.sleep(0.1)
     EXTRA_GLOBALS.file_is_reading_or_writing = True  # 文件被访问
     with open(file=ExportFilePath, mode="w", encoding='utf-8') as json_file:
-        json_file.write(json.dumps(data_list, indent=4, ensure_ascii=False))
+        json_file.write(json.dumps(data_list_2, indent=4, ensure_ascii=False))
     EXTRA_GLOBALS.file_is_reading_or_writing = False  # 文件已解锁
+
 
 if __name__ == '__main__':
     main()
