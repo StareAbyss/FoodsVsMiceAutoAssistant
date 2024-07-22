60bd5f2c4d07ec573607de0d95a245b18ada8df4
syfoud <xing12130@outlook.com>
2024-07-22 15:57:50 +0800

    引入yolo代码

---
+++ yolo/model/onnxdetect.py
@@ -0,0 +1,75 @@
+import argparse
+import cv2.dnn
+import numpy as np
+
+'''
+注意：如果你推理自己的模型，以下类别需要改成你自己的具体类别
+'''
+# coco类别
+CLASSES = {0: 'shell', 1: 'flypig', 2: 'pope', 3: 'Vali', 4: 'wave', 5: 'GodWind', 6: 'skull' }# 对应6种特殊老鼠与波次
+colors = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+# 绘制
+def draw_bounding_box(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+    label = f'{CLASSES[class_id]} ({confidence:.2f})'
+    color = colors[class_id]
+    # 绘制矩形框
+    cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
+    # 绘制类别
+    cv2.putText(img, label, (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+
+def main(onnx_model, input_image):
+    # 使用opencv读取onnx文件
+    model: cv2.dnn.Net = cv2.dnn.readNetFromONNX(onnx_model)
+    # 读取原图
+    original_image: np.ndarray = cv2.imread(input_image)
+    [height, width, _] = original_image.shape
+    length = max((height, width))
+    image = np.zeros((length, length, 3), np.uint8)
+    image[0:height, 0:width] = original_image
+    scale = length / 640 # 缩放比例
+    # 设置模型输入
+    blob = cv2.dnn.blobFromImage(image, scalefactor=1 / 255, size=(640, 640), swapRB=True)
+    model.setInput(blob)
+    # 推理
+    outputs = model.forward() # output: 1 X 8400 x 84
+    outputs = np.array([cv2.transpose(outputs[0])])
+    rows = outputs.shape[1]
+
+    boxes = []
+    scores = []
+    class_ids = []
+    # outputs有8400行，遍历每一行，筛选最优检测结果
+    for i in range(rows):
+        # 找到第i个候选目标在80个类别中，最可能的类别
+        classes_scores = outputs[0][i][4:] # classes_scores:80 X 1
+        (minScore, maxScore, minClassLoc, (x, maxClassIndex)) = cv2.minMaxLoc(classes_scores)
+        if maxScore >= 0.25:
+            box = [
+                # cx cy w h  -> x y w h
+                outputs[0][i][0] - (0.5 * outputs[0][i][2]), outputs[0][i][1] - (0.5 * outputs[0][i][3]),
+                outputs[0][i][2], outputs[0][i][3]]
+            boxes.append(box) #边界框
+            scores.append(maxScore) # 置信度
+            class_ids.append(maxClassIndex) # 类别
+    # opencv版最极大值抑制
+    result_boxes = cv2.dnn.NMSBoxes(boxes, scores, 0.25, 0.45, 0.5)
+
+    for i in range(len(result_boxes)):
+        index = result_boxes[i]
+        box = boxes[index]
+        draw_bounding_box(original_image, class_ids[index], scores[index], round(box[0] * scale), round(box[1] * scale),
+                          round((box[0] + box[2]) * scale), round((box[1] + box[3]) * scale))
+    cv2.imshow('image', original_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', default='mouse.onnx', help='Input your onnx model.')
+    parser.add_argument('--img', default=str('bus.jpg'), help='Path to input image.')
+    args = parser.parse_args()
+    main(args.model, args.img)
+
