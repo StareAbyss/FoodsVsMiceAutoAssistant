4e115df225eb3612978c43f3af7c6df9ac333679
syfoud <xing12130@outlook.com>
2024-07-26 14:10:55 +0800

    extra beta0.0.1 实现多开进程用于识图，并在一定程度上修复了结束战斗按钮不防熊的问题

--- function/yolo/onnxdetect.py
+++ function/yolo/onnxdetect.py
@@ -1,8 +1,10 @@
 import argparse
 import cv2.dnn
 import numpy as np
-import os
-
+import datetime
+import random
+from math import floor
+from function.globals.get_paths import PATHS
 '''
 注意：如果你推理自己的模型，以下类别需要改成你自己的具体类别
 '''
@@ -20,18 +22,19 @@ def draw_bounding_box(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
     cv2.putText(img, label, (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
 
-def get_mouse_position(onnx_model, input_image):
+def get_mouse_position(input_image):
     # 使用opencv读取onnx文件
+    onnx_model = PATHS["model"]+"/mouse.onnx"
     model: cv2.dnn.Net = cv2.dnn.readNetFromONNX(onnx_model)
     # 读取原图
-    original_image: np.ndarray = cv2.imread(input_image)
+    original_image = input_image[:, :, :3]#去除阿尔法通道
     [height, width, _] = original_image.shape
     length = max((height, width))
     image = np.zeros((length, length, 3), np.uint8)
     image[0:height, 0:width] = original_image
     scale = length / 640 # 缩放比例
     # 设置模型输入
-    blob = cv2.dnn.blobFromImage(image, scalefactor=1 / 255, size=(640, 640), swapRB=True)
+    blob = cv2.dnn.blobFromImage(image, scalefactor=1 / 255, size=(640, 640), swapRB=False)#通道是匹配的，不用交换红蓝
     model.setInput(blob)
     # 推理
     outputs = model.forward() # output: 1 X 8400 x 84
@@ -56,33 +59,36 @@ def get_mouse_position(onnx_model, input_image):
             class_ids.append(maxClassIndex) # 类别
     # opencv版最极大值抑制
     result_boxes = cv2.dnn.NMSBoxes(boxes, scores, 0.25, 0.45, 0.5)
-    annotated_image = original_image.copy()
     for i in range(len(result_boxes)):
         index = result_boxes[i]
         box = boxes[index]
-        draw_bounding_box(annotated_image, class_ids[index], scores[index], round(box[0] * scale), round(box[1] * scale),
-                          round((box[0] + box[2]) * scale), round((box[1] + box[3]) * scale))
-    annotated_image=cv2.resize(annotated_image, (960,540))
-    cv_show('test',annotated_image)
-    need_write=True
+    # annotated_image = original_image.copy()
+    #     draw_bounding_box(annotated_image, class_ids[index], scores[index], round(box[0] * scale), round(box[1] * scale),
+    #                       round((box[0] + box[2]) * scale), round((box[1] + box[3]) * scale))
+    # annotated_image=cv2.resize(annotated_image, (960,540))
+    # cv_show('test',annotated_image)
+    need_write=True#是否保存图片及对应标签，未来将对接前端
     if need_write:
-        cv_write(input_image,original_image,result_boxes,class_ids,scores,boxes,scale)
-def cv_write(input_image,original_image,result_boxes,class_ids,scores,boxes,scale):
-        output_base_path = os.getcwd()
-        img_base_name = os.path.splitext(os.path.basename(input_image))[0]
-        output_img_path = f"{output_base_path}/{img_base_name}.png"
-        output_txt = f"{output_base_path}/{img_base_name}.txt"
-        cv2.imwrite(output_img_path,original_image)
-        with open(output_txt, 'w') as f:
-            for i in range(len(result_boxes)):
-                index = result_boxes[i]
-                class_id = class_ids[index]
-                score = scores[index]
-                box = boxes[index]
-                x, y, w, h = voc_to_yolo(original_image.shape, [box[0]*scale,box[1]*scale,box[2]*scale,box[3]*scale])
-                # x, y, w, h = box[0]*scale,box[1]*scale,box[2]*scale,box[3]*scale
-                # 写入格式：类别,x,y,w,h
-                f.write(f"{class_id} {x } {y } {w } {h }\n")
+        cv_write(original_image,result_boxes,class_ids,scores,boxes,scale)
+    return boxes,class_ids#返回边界框及类别用作进一步处理
+
+def cv_write(original_image,result_boxes,class_ids,scores,boxes,scale):#此函数用于保存标签label及对应数据图片
+    output_base_path = PATHS["logs"]+"\\yolo_output"
+    now = datetime.datetime.now()
+    timestamp = now.strftime("%Y%m%d%H%M%S")
+    rand_num = str(floor(random.random() * 1000000))
+    output_img_path = f"{output_base_path}/images/{timestamp}_{rand_num}.png"
+    output_txt = f"{output_base_path}/labels/{timestamp}_{rand_num}.txt"
+    cv2.imwrite(output_img_path, original_image)
+    with open(output_txt, 'w') as f:
+        for i in range(len(result_boxes)):
+            index = result_boxes[i]
+            class_id = class_ids[index]
+            score = scores[index]
+            box = boxes[index]
+            x, y, w, h = voc_to_yolo(original_image.shape,
+                                     [box[0] * scale, box[1] * scale, box[2] * scale, box[3] * scale])
+            f.write(f"{class_id} {x} {y} {w} {h}\n")
 
 
 
@@ -113,7 +119,7 @@ def cv_show(name,img):#图片展示
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', default='mouse.onnx', help='Input your onnx model.')
-    parser.add_argument('--img', default=str('0002_27.jpg'), help='Path to input image.')
+    parser.add_argument('--img', default=str('0002_10.jpg'), help='Path to input image.')
     args = parser.parse_args()
     get_mouse_position(args.model, args.img)
 
