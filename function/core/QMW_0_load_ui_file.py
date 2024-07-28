ec0ca212ca0f7d19e07867f8709b311107d15a20
StareAbyss <815204388@qq.com>
2024-07-28 16:59:30 +0800

    v1.4.1-beta.1 杂项.

--- function/core/QMW_0_load_ui_file.py
+++ function/core/QMW_0_load_ui_file.py
@@ -40,7 +40,7 @@ class QMainWindowLoadUI(QMainWindow):
         if font_id != -1:
             font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
             self.font = QFont(font_family, 11)
-            self.setFont( self.font)
+            self.setFont(self.font)
 
     def closeEvent(self, event):
         """
