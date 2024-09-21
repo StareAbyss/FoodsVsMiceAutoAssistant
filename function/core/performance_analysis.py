11e48f80f26712ab4edb3878a7ec5bab3bded7dd
syfoud <xing12130@outlook.com>
2024-09-21 15:06:06 +0800

    v1.5.0 beta3 完成性能分析实现

--- function/core/performance_analysis.py
+++ function/core/performance_analysis.py
@@ -1,59 +1,66 @@
 import asyncio
 import os
+import threading
+
 import psutil
 
-async def monitor_io(process_ids, stop_event):
-    while not stop_event.is_set():
-        total_read_bytes = 0
-        total_write_bytes = 0
+async def monitor_io(process_ids, stop_event,main_window):
+    while True:
+        if not stop_event.is_set():
+            total_read_bytes = 0
+            total_write_bytes = 0
 
-        for process_id in process_ids:
-            try:
-                process = psutil.Process(process_id)
-                io_counters = process.io_counters()
-                total_read_bytes += io_counters.read_bytes
-                total_write_bytes += io_counters.write_bytes
-            except psutil.NoSuchProcess:
-                continue
-
-        print(f"总读取字节数: {total_read_bytes / 1024 / 1024:.2f} MB")
-        print(f"总写入字节数: {total_write_bytes / 1024 / 1024:.2f} MB")
+            for process_id in process_ids:
+                try:
+                    process = psutil.Process(process_id)
+                    io_counters = process.io_counters()
+                    total_read_bytes += io_counters.read_bytes
+                    total_write_bytes += io_counters.write_bytes
+                except psutil.NoSuchProcess:
+                    continue
+            main_window.read_bytes_label.setText(f"{total_read_bytes / 1024 / 1024:.2f} MB")
+            main_window.write_bytes_label.setText(f"{total_write_bytes / 1024 / 1024:.2f} MB")
+            # print(f"总读取字节数: {total_read_bytes / 1024 / 1024:.2f} MB")
+            # print(f"总写入字节数: {total_write_bytes / 1024 / 1024:.2f} MB")
         await asyncio.sleep(1)
 
 
-async def monitor_memory(process_ids, stop_event):
-    while not stop_event.is_set():
-        total_memory_rss = 0
-        total_memory_percent = 0
+async def monitor_memory(process_ids, stop_event,main_window):
+    while True:
+        if not stop_event.is_set():
+            total_memory_rss = 0
+            total_memory_percent = 0
 
-        for process_id in process_ids:
-            try:
-                process = psutil.Process(process_id)
-                memory_info = process.memory_info()
-                memory_percent = process.memory_percent()
-                total_memory_rss += memory_info.rss
-                total_memory_percent += memory_percent
-            except psutil.NoSuchProcess:
-                continue
-
-        print(f"总内存使用量: {total_memory_rss / (1024 * 1024):.2f} MB")
-        print(f"总内存使用率: {total_memory_percent:.2f}%")
+            for process_id in process_ids:
+                try:
+                    process = psutil.Process(process_id)
+                    memory_info = process.memory_info()
+                    memory_percent = process.memory_percent()
+                    total_memory_rss += memory_info.rss
+                    total_memory_percent += memory_percent
+                except psutil.NoSuchProcess:
+                    continue
+            main_window.total_memory_rss_label.setText(f"{total_memory_rss / (1024 * 1024):.2f} MB")
+            main_window.total_memory_percent_label.setText(f"{total_memory_percent:.2f}%")
+            # print(f"总内存使用量: {total_memory_rss / (1024 * 1024):.2f} MB")
+            # print(f"总内存使用率: {total_memory_percent:.2f}%")
         await asyncio.sleep(1)
 
 
-async def monitor_cpu(process_ids, stop_event):
-    while not stop_event.is_set():
-        total_cpu_percent = 0
+async def monitor_cpu(process_ids, stop_event,main_window):
+    while True:
+        if not stop_event.is_set():
+            total_cpu_percent = 0
 
-        for process_id in process_ids:
-            try:
-                process = psutil.Process(process_id)
-                cpu_percent = process.cpu_percent(interval=1)
-                total_cpu_percent += cpu_percent
-            except psutil.NoSuchProcess:
-                continue
-
-        print(f"总CPU使用率: {total_cpu_percent:.2f}%")
+            for process_id in process_ids:
+                try:
+                    process = psutil.Process(process_id)
+                    cpu_percent = process.cpu_percent(interval=1)
+                    total_cpu_percent += cpu_percent
+                except psutil.NoSuchProcess:
+                    continue
+            main_window.total_cpu_percent_label.setText(f"{total_cpu_percent:.2f}%")
+            # print(f"总CPU使用率: {total_cpu_percent:.2f}%")
         await asyncio.sleep(1)
 
 
@@ -82,17 +89,27 @@ async def analysis(main_window):
 
     async def check_tab_selection():
         while True:
-            if main_window.tabWidget.currentIndex() == 0:#选中了性能分析tab
+            if main_window.tabWidget.currentIndex() == 5:#选中了性能分析tab
                 stop_event.clear()  # 清除事件标志，允许监控
             else:
                 stop_event.set()  # 设置事件标志，停止监控
             await asyncio.sleep(1)  # 检查间隔
 
     tasks = [
-        monitor_io(process_ids, stop_event),
-        monitor_memory(process_ids, stop_event),
-        monitor_cpu(process_ids, stop_event),
+        monitor_io(process_ids, stop_event,main_window),
+        monitor_memory(process_ids, stop_event,main_window),
+        monitor_cpu(process_ids, stop_event,main_window),
         check_tab_selection()  # 新增任务，用于检测tab选择状态
     ]
 
     await asyncio.gather(*tasks)
+
+def run_analysis_in_thread(main_window):
+    def target_function():
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(analysis(main_window))
+        loop.close()
+
+    thread = threading.Thread(target=target_function, daemon=True)
+    thread.start()
