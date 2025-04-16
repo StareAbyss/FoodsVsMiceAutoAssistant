99eb8198245681635ce8fd656b7c745e03a88039
StareAbyss <815204388@qq.com>
2025-04-16 18:18:00 +0800

    fix(关卡进入): 修复蛇来运转无法进入的问题, 错误原因, 将时间限制为了7:00-7:30, 而非 19:00-19:30

--- function/core/faa/faa_core.py
+++ function/core/faa/faa_core.py
@@ -176,11 +176,11 @@ class FAABase:
 
         if stage_0 == "CZ":
             # Chinese Zodiac 生肖关卡, 特殊关卡.
-            # 北京时间(注意时区) 周4567的 7点到7点半可以进入
+            # 北京时间(注意时区) 周4567的 晚上7点到7点半可以进入
             # 先不做星期检测, 只做时间段检测, 节假日比较迷惑
             beijing_tz = pytz.timezone('Asia/Shanghai')
             now = datetime.now(beijing_tz)
-            return now.hour == 7 and 0 <= now.minute < 30
+            return now.hour == 19 and 0 <= now.minute < 30
 
         return True
 
