3c6e398dd11bb0450025bfd6d32f1df84416cd74
StareAbyss <815204388@qq.com>
2025-12-19 13:03:08 +0800

    feat(UI): 添加点击后自动布局360窗口功能, 强迫症狂喜

--- function/core/qmw_3_service.py
+++ function/core/qmw_3_service.py
@@ -184,8 +184,11 @@ class QMainWindowService(QMainWindowLoadSettings):
         self.CurrentPlan.currentIndexChanged.connect(self.opt_to_ui_todo_plans)
 
         # 隐藏(拖动)窗口到屏幕视图外 函数绑定
-        self.Button_Hide.clicked.connect(self.click_btn_hide_window)
+        self.Button_WindowHide.clicked.connect(self.click_btn_hide_window)
         self.game_window_is_hide = False
+
+        # 调整窗口大小
+        self.Button_WindowReSize.clicked.connect(self.click_btn_batch_resize_window)
 
         # 重置卡片状态自学习记忆
         self.ResetCardStatusMemoryButton.clicked.connect(self.click_btn_reset_card_status_memory)
@@ -849,6 +852,86 @@ class QMainWindowService(QMainWindowLoadSettings):
                 # win32gui.ShowWindow(handle,0)
                 # 也许有一天能写出真正的老板键 大概
             self.game_window_is_hide = True
+
+    def click_btn_batch_resize_window(self):
+        """
+        调整窗口大小并设置窗口位置
+        :return:
+        """
+
+        # 获取窗口名称
+        channel_1p, channel_2p = get_channel_name(
+            game_name=self.opt["base_settings"]["game_name"],
+            name_1p=self.opt["base_settings"]["name_1p"],
+            name_2p=self.opt["base_settings"]["name_2p"])
+
+        handles = {
+            1: faa_get_handle(channel=channel_1p, mode="360"),
+            2: faa_get_handle(channel=channel_2p, mode="360")}
+        width = int(955 * self.zoom_rate)
+        height = int(668 * self.zoom_rate)
+
+        # 获取屏幕工作区域大小
+        user32 = ctypes.windll.user32
+        screen_width = user32.GetSystemMetrics(0)
+        screen_height = user32.GetSystemMetrics(1)
+
+        if height * 2 <= screen_height:
+
+            # 第一个窗口放置在屏幕右上角,最小大小
+            win32gui.SetWindowPos(
+                handles[1],
+                win32con.HWND_TOP,
+                screen_width - width,  # X坐标：屏幕宽度减去窗口宽度
+                0,  # Y坐标：顶部对齐
+                width,
+                height,
+                win32con.SWP_SHOWWINDOW
+            )
+
+            if handles[2]:
+                # 第二个窗口放置在第一个窗口下方（紧贴）,最小大小
+                win32gui.SetWindowPos(
+                    handles[2],
+                    win32con.HWND_TOP,
+                    screen_width - width,  # X坐标
+                    height,  # Y坐标
+                    width,
+                    height,
+                    win32con.SWP_SHOWWINDOW
+                )
+        else:
+            if handles[2]:
+                # 顶部居中，宽度占满屏幕，高度最小
+                win32gui.SetWindowPos(
+                    handles[1],
+                    win32con.HWND_TOP,
+                    0,  # X坐标：屏幕宽度减去窗口宽度
+                    0,  # Y坐标：顶部对齐
+                    int(screen_width/2),
+                    height,
+                    win32con.SWP_SHOWWINDOW
+                )
+                win32gui.SetWindowPos(
+                    handles[2],
+                    win32con.HWND_TOP,
+                    int(screen_width/2),  # X坐标：屏幕宽度减去窗口宽度
+                    0,  # Y坐标：顶部对齐
+                    int(screen_width/2),
+                    height,
+                    win32con.SWP_SHOWWINDOW
+                )
+            else:
+                # 第一个窗口放置在屏幕右上角,最小大小
+                win32gui.SetWindowPos(
+                    handles[1],
+                    win32con.HWND_TOP,
+                    screen_width - width,  # X坐标：屏幕宽度减去窗口宽度
+                    0,  # Y坐标：顶部对齐
+                    width,
+                    height,
+                    win32con.SWP_SHOWWINDOW
+                )
 
     """打开其他窗口"""
 
